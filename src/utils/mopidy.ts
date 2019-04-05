843d08700eeaa10d63b852da2d6d0e413d2c1918
Marc Löhe <marc@marcloehe.de>
2019-04-06 01:42:40 +0200

    Add mopidy stub

---
+++ src/utils/mopidy.ts
@@ -0,0 +1,7 @@
+/*
+Playing a spotify playlist in web ui
+{"method":"core.tracklist.clear","params":{},"jsonrpc":"2.0","id":329}
+{"method":"core.tracklist.add","params":{"uri":"spotify:track:49MWSiZnKcHA3AVDfahSJv","at_position":0},"jsonrpc":"2.0","id":100}
+{"method":"core.tracklist.clear","params":{},"jsonrpc":"2.0","id":329}
+{"method":"core.playback.play","params":{"tlid":27},"jsonrpc":"2.0","id":101}
+ */
