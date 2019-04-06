d52585f5475a1845fe3ee13517bf028dee1289f5
Marc Löhe <marc@marcloehe.de>
2019-04-06 03:07:26 +0200

    feat: make WEBSOCKET_URL configurable

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import setupGpio from "./gpio";
 import MopidyClient from "./MopidyClient";
 import { spotifyButtonMapHelpers } from "./utils/buttonMapHelpers";
 
+const { WEBSOCKET_URL: webSocketUrl = "ws://localhost:6680/mopidy/ws/" } = process.env;
+
+console.log("Starting mopidy-buttons…", { webSocketUrl });
+
 (async () => {
   let mopidy: MopidyClient;
   try {
     mopidy = new MopidyClient({
-      webSocketUrl: "ws://lil:6680/mopidy/ws/"
+      webSocketUrl
     });
     await mopidy.init();
     // await mopidy.playSpotifyPlaylist("spotify:track:49MWSiZnKcHA3AVDfahSJv");
